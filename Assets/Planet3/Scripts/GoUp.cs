137a09cff0a9786a06d265a46de0c6730329aad4
yiyaonie <nieyiyao@gmail.com>
2016-04-11 16:51:27 -0700

    planet3-2

---
+++ Assets/Planet3/Scripts/GoUp.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GoUp : MonoBehaviour {
+	private Animator anim;
+	public GameObject o;
+	private Animator anim2;
+	public GameObject o2;
+	private Animator anim3;
+
+	// Use this for initialization
+	void Start () {
+		anim = o.GetComponent<Animator>();
+		anim2 = o2.GetComponent<Animator>();
+		anim3 = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void actionUp(){
+		anim.SetBool ("up", true);
+		anim2.SetBool("up",true);
+		anim3.SetBool("roseFallReverse", true);
+	}
+
+}
