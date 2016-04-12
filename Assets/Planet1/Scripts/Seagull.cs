1e244989d9dd5d69c61cbce32ee7a6c90ca8a845
Tonghe Wang <hetong583@qq.com>
2016-04-12 16:40:28 -0700

    add group of seagull

---
+++ Assets/Planet1/Scripts/Seagull.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Seagull : MonoBehaviour {
+
+    RangeAttribute delay = new RangeAttribute(0, 4);
+    Animator anim;
+	// Use this for initialization
+	void Start () {
+        anim = GetComponent<Animator>();
+        anim.speed = 0;
+        StartCoroutine(animDelay());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    IEnumerator animDelay()
+    {
+        yield return new WaitForSeconds(Random.Range(delay.min, delay.max));
+        anim.speed = 1.2f;
+    }
+}
---
+++ Assets/Planet1/Scripts/SeagullGroup.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SeagullGroup : MonoBehaviour {
+
+    float movespeed = 10;
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(move());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    IEnumerator move()
+    {
+        yield return new WaitForSeconds(15);
+        while (transform.position.z < 1000)
+        {
+            Vector3 newpos = transform.position;
+            newpos.z += movespeed * Time.deltaTime;
+            transform.position = newpos;
+            yield return new WaitForEndOfFrame();
+        }
+    }
+}
