da9d8f947ecb920cd8cbf1d7e2c8d526934765df
elevenyao <yirlinya@163.com>
2016-04-13 00:04:29 -0700

    planet2

---
+++ Assets/Planet2/script/parent.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class parent : MonoBehaviour {
+	public GameObject o;
+	public GameObject o2;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	void ChangeParent(){
+		o2.transform.parent = o.transform;
+	}
+}
---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour {
+
+    private string currentTime;
+    public Text timeText;
+    float setTime = 30.0f;
+
+
+	// Use this for initialization
+	void Start () {
+        timeText = GameObject.Find("timerUIText").GetComponent<Text>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (setTime <= 0)
+        {
+            setTime = 0;
+            timeText.text = "LALALA";
+        }
+        else
+        {
+            setTime -= Time.deltaTime;
+            currentTime = string.Format("{0:0.0}", setTime);
+
+            timeText.text = "Time Left: " + currentTime;
+        }
+	}
+}
