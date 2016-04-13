4d7d8e85fab2334dc2d57c8990879f72b69d9269
Tonghe Wang <hetong583@qq.com>
2016-04-12 23:57:34 -0700

    sunlight disappear

--- Assets/Planet1/Scripts/SkyManager.cs
+++ Assets/Planet1/Scripts/SkyManager.cs
@@ -1,14 +1,19 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityStandardAssets.Water;
 
 public class SkyManager : MonoBehaviour {
 
+    public GameObject water;
     Material skyMaterial;
+    Material waterMaterial;
+
 	// Use this for initialization
 	void Start () {
         StartCoroutine(dark());
         skyMaterial = RenderSettings.skybox;
         skyMaterial.SetFloat("_SkyBlend", 0);
+        waterMaterial = water.GetComponent<WaterBase>().sharedMaterial;
 	}
 	
 	// Update is called once per frame
@@ -23,6 +28,7 @@ public class SkyManager : MonoBehaviour {
 
         float timer = 0;
         float fog = RenderSettings.fogDensity;
+        Color specular = waterMaterial.GetColor("_SpecularColor");
         while (skyMaterial.GetFloat("_SkyBlend") < end)
         {
             Debug.Log(timer / duration);
@@ -30,6 +36,9 @@ public class SkyManager : MonoBehaviour {
             skyMaterial.SetFloat("_SkyBlend", blend);
 
             RenderSettings.fogDensity = Mathf.Lerp(fog, 0, timer / duration);
+
+            Color newcolor = Color.Lerp(specular, Color.clear, timer / duration);
+            waterMaterial.SetColor("_SpecularColor", newcolor);
             timer += Time.deltaTime;
             yield return new WaitForEndOfFrame();
         }
