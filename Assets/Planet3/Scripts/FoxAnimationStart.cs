b73348a558a9015ceb1eafde9ea11df3560952cf
yiyaonie <nieyiyao@gmail.com>
2016-04-12 18:34:24 -0700

    k

--- Assets/Planet3/Scripts/FoxAnimationStart.cs
+++ Assets/Planet3/Scripts/FoxAnimationStart.cs
@@ -4,6 +4,8 @@ using System.Collections;
 public class FoxAnimationStart : MonoBehaviour {
 	private Animator anim;
 	public GameObject o;
+	public AudioSource part1;
+	public AudioSource part2;
 	// Use this for initialization
 	void Start () {
 		anim = o.GetComponent<Animator>();
@@ -14,7 +16,17 @@ public class FoxAnimationStart : MonoBehaviour {
 	
 	}
 
+	public void ActionPart1(){
+		part1.Play ();
+	}
+
 	public void ActionFox(){
 		anim.SetBool ("foxLookBack", true);
+
+	}
+
+	public void ActionPart2(){
+		part2.Play ();
+	
 	}
 }
---
+++ Assets/Planet3/Scripts/audioLast.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class audioLast : MonoBehaviour {
+	public AudioSource part3;
+	public AudioSource part4;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void Action3(){
+		part3.Play ();
+	}
+
+	public void Action4(){
+		part4.Play ();
+	}
+}
