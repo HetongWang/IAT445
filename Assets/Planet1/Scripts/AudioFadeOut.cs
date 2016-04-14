47969dfb69db47f897c3ca473aceec4e722c6298
Tonghe Wang <hetong583@qq.com>
2016-04-13 21:49:42 -0700

    add audio fade out

---
+++ Assets/Planet1/Scripts/AudioFadeOut.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AudioFadeOut : MonoBehaviour {
+
+    public AudioSource ap;
+    public float duration;
+    float volume;
+
+	// Use this for initialization
+	void Start () {
+        volume = ap.volume;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void FadeOut()
+    {
+        StartCoroutine(beginFadeOut());
+    }
+
+    IEnumerator beginFadeOut()
+    {
+        while (ap.volume > 0)
+        {
+            ap.volume -= volume / duration * Time.deltaTime;
+            yield return new WaitForEndOfFrame();
+        }
+        ap.volume = 0;
+    }
+}
